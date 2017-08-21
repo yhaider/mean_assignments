20b31ad528fd1b734ab764d37de558c0ddb3c51c
Yasmeen <yhaider@uchicago.edu>
2017-08-20 23:02:55 -0400

    added login functionality and bcrypt

--- full_mean/mean-login-registration/server/controllers/users.js
+++ full_mean/mean-login-registration/server/controllers/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 	login: function (req, res) {
@@ -7,18 +8,43 @@ module.exports = {
             if(err){
                 return res.status(500).json(err);
             }
-            return res.json(user);
+			else{
+				if(usercheck.password != req.body.password){
+					let error = "Invalid password."
+					return res.status(401).json(error)
+				}
+
+				else{
+					console.log('success')
+					return res.json(user);
+				}
+			}
+
         })
+
 
 	},
 
 	create: function (req, res) {
-		let user = new User(req.body);
-        user.save((err) => {
-            if(err){
-                return res.status(401).json(err);
-            }
-            return res.json(user);
-        });
+		User.find({email:req.body.email}, function(err,user){
+			if(user.length > 0){
+				return res.status(400).json("Email already registered.")
+			}
+			else{
+				let newUser = new User(req.body);
+				newUser.password = bcrypt.hashSync(newUser.password, bcrypt.genSaltSync(8));
+				newUser.save(function(err){
+					if(err){
+						console.warn(err);
+						errors.push(err);
+						return res.status(400).json({errors: errors});
+					}
+					else {
+						console.log(newUser.password);
+						return res.json(newUser);
+					}
+				})
+			}
+		})
 	}
 }
